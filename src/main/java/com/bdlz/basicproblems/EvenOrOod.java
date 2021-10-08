e5d53f1754377e5e8c2cf6153bb6808af208ac80
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-08 20:33:56 +0530

    Added: Even or ood program

---
+++ src/main/java/com/bdlz/basicproblems/EvenOrOod.java
@@ -0,0 +1,20 @@
+package com.bdlz.basicproblems;
+
+import java.util.Scanner;
+
+public class EvenOrOod {
+    public static void main(String[] args) {
+        //Variable Declaration
+        int number;
+        System.out.println("Enter the integer value to Check even or odd");
+        //To read input from Console
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextInt();
+        scanner.close();
+        if ((number % 2) == 0) {
+            System.out.println("Number : " + number + " is Even.");
+        } else {
+            System.out.println("Number : " + number + " is Odd");
+        }
+    }
+}
